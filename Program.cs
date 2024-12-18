dcd3f74e36d74d1c3c1bc297952b964c2bb87565
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2024-12-18 09:40:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read(); 
+        }
+        static void Func()
+        {
+            for (int x = 1; x <= 18; x++)
+            {
+                Console.WriteLine("#");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}

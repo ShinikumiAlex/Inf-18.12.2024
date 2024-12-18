c77d479936ccf852812388c5bbe559951749619a
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2024-12-18 09:23:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("привет");
+            }
+        }
+    }
+}
